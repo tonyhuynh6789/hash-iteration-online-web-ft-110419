baf2fa3418b5427a9ffe30fd88e2b05f0edbe02b
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-09 22:17:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,8 +10,8 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-  winner = winner
-  end 
+  winner = name
+  end
   end
   winner
 end 
